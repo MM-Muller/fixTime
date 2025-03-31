03b5dc618bcee70ad1004aa314f16092ebd64d4c
Matheus Müller <MM-Muller@users.noreply.github.com>
2025-03-31 12:23:43 -0300

    arrumando

--- DOCUMENTAÇÃO/testeCRUD/conecta_db.php
+++ DOCUMENTAÇÃO/testeCRUD/conecta_db.php
@@ -2,7 +2,7 @@
 	function conecta_db(){
 		$db_name = "db_teste";
 		$user    = "root";
-		$pass    = "Maverick14042005";
+		$pass    = "";
 		$server  = "localhost:3306";
 		$conexao = new mysqli($server, $user, $pass, $db_name);
 		return $conexao;
