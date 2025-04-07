268666ae704ebeb2c8e16e292d622b3fb01600e9
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-04-07 11:11:58 -0300

    Adiciona botões de editar e excluir perfil na página de perfil

--- PROJETO/src/views/main-page/perfil.php
+++ PROJETO/src/views/main-page/perfil.php
@@ -122,31 +122,50 @@
 
     <div class=" lg:ml-64 lg:py-10 py-4 lg:px-32 px-8 ">
 
-        <div class="p-10 bg-white border border-gray-200 rounded-lg shadow-sm">
+        <div class="p-8 bg-white border border-gray-200 rounded-lg shadow-sm">
             <div class="space-y-7">
                 <div class="">
                     <label for="nome-perfil" class="block mb-1 text-sm font-medium text-gray-900 ">Nome</label>
-                    <input type="text" id="nome-perfil" value="<?php echo htmlspecialchars($user_data['nome_usuario']); ?>" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2 cursor-not-allowed" disabled/>
+                    <input type="text" id="nome-perfil" value="<?php echo htmlspecialchars($user_data['nome_usuario']); ?>" class="cursor-not-allowed bg-gray-50 border-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 outline-none" disabled/>
                 </div> 
     
                 <div class="">
                     <label for="cpf-perfil" class="block mb-1 text-sm font-medium text-gray-900 ">CPF</label>
-                    <input type="text" id="cpf-perfil" value="<?php echo htmlspecialchars($user_data['cpf']); ?>" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2 cursor-not-allowed" disabled/>
+                    <input type="text" id="cpf-perfil" value="<?php echo htmlspecialchars($user_data['cpf']); ?>" class="cursor-not-allowed bg-gray-50 border-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 outline-none" disabled/>
                 </div> 
     
                 <div class="">
                     <label for="telefone-perfil" class="block mb-1 text-sm font-medium text-gray-900 ">Número de telefone</label>
-                    <input type="text" id="telefone-perfil" value="<?php echo htmlspecialchars($user_data['telefone_usuario']); ?>" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2 cursor-not-allowed" disabled/>
+                    <input type="text" id="telefone-perfil" value="<?php echo htmlspecialchars($user_data['telefone_usuario']); ?>" class="cursor-not-allowed bg-gray-50 border-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 outline-none" disabled/>
                 </div> 
     
                 <div class="">
                     <label for="email-perfil" class="block mb-1 text-sm font-medium text-gray-900 ">Email</label>
-                    <input type="text" id="email-perfil" value="<?php echo htmlspecialchars($user_data['email_usuario']); ?>" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2 cursor-not-allowed" disabled/>
+                    <input type="text" id="email-perfil" value="<?php echo htmlspecialchars($user_data['email_usuario']); ?>" class=" cursor-not-allowed bg-gray-50 border-2 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2 outline-none" disabled/>
                 </div> 
                 
             </div>
 
+            <div class="lg:gap-6 gap-4 items-center grid grid-cols-6 mt-6">
+                    <button type="button" class="text-white inline-flex items-center justify-center gap-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center cursor-pointer col-span-3">
+                        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z"></path>
+                            <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd"></path>
+                        </svg>
+                        Editar
+                    </button>     
+                
+                    <button type="button" class="inline-flex items-center justify-center gap-2 text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center cursor-pointer col-span-3">
+                        <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd"></path>
+                        </svg>
+                        Excluir perfil
+                    </button>
+                </div>
+
         </div>
+
+        
         
         
     </div>
