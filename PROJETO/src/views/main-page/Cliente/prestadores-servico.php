d2960183ee169a008299d54c35a00f817b392f0e
Vinicius Padilha <viniciusdarosapadilha@gmail.com>
2025-05-19 08:49:29 -0300

    Altera estilo na pesquisa do prestador de serviço

--- PROJETO/src/views/main-page/Cliente/prestadores-servico.php
+++ PROJETO/src/views/main-page/Cliente/prestadores-servico.php
@@ -110,12 +110,12 @@ $stmt->close();
 
     </aside>
 
-    <div class=" lg:ml-64 p-10 ">
+    <div class=" lg:ml-64 lg:px-10 lg:py-6">
         <div class="flex justify-center items-center">
-            <h1 class="mb-3 text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">Oficinas Parceiras</h1>
+            <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">Oficinas Parceiras</h1>
         </div>
 
-        <hr class=" h-px my-8 bg-gray-200 border-0">
+        <hr class="h-px my-8 bg-gray-200 border-0">
 
         <?php
         $filter = isset($_GET['filter']) ? $_GET['filter'] : '';
