04b007652325485fa7dd9095098a5c68c5108ffa
Vinicius <viniciusdarosapadilha@gmail.com>
2025-06-01 17:07:27 -0300

    Adiciona lógica avaliação

--- PROJETO/src/views/main-page/Cliente/meus-agendamentos.php
+++ PROJETO/src/views/main-page/Cliente/meus-agendamentos.php
@@ -261,42 +261,47 @@ $conexao->close();
                             <input type="hidden" name="id_oficina" value="<?= $servico['id_oficina'] ?>">
                             <input type="hidden" name="estrelas" id="estrelasInput" value="0">
 
-                    </div>
                             <div class="grid min-h-[140px] w-full place-items-center overflow-x-scroll rounded-lg p-6 lg:overflow-visible">
-                              <div class="inline-flex items-center"><span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-                                    fill="currentColor" class="w-6 h-6 text-yellow-600 cursor-pointer">
-                                    <path fill-rule="evenodd"
-                                      d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                                      clip-rule="evenodd"></path>
-                                  </svg></span><span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                                    class="w-6 h-6 text-yellow-600 cursor-pointer">
-                                    <path fill-rule="evenodd"
-                                      d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                                      clip-rule="evenodd"></path>
-                                  </svg></span><span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                                    class="w-6 h-6 text-yellow-600 cursor-pointer">
-                                    <path fill-rule="evenodd"
-                                      d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                                      clip-rule="evenodd"></path>
-                                  </svg></span><span><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"
-                                    class="w-6 h-6 text-yellow-600 cursor-pointer">
-                                    <path fill-rule="evenodd"
-                                      d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
-                                      clip-rule="evenodd"></path>
-                                  </svg></span><span><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
-                                    stroke="currentColor" class="w-6 h-6 cursor-pointer text-gray-500">
-                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                      d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z">
-                                    </path>
-                                  </svg></span></div>
+                                <div class="inline-flex items-center gap-2" id="estrelasContainer">
+                                    <!-- Estrela 1 -->
+                                    <svg data-estrela="1" class="estrela-btn w-8 h-8 cursor-pointer text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                          d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z">
+                                                        </path>
+                                    </svg>
+                                    <!-- Estrela 2 -->
+                                    <svg data-estrela="2" class="estrela-btn w-8 h-8 cursor-pointer text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                          d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z">
+                                                        </path>
+                                    </svg>
+                                    <!-- Estrela 3 -->
+                                    <svg data-estrela="3" class="estrela-btn w-8 h-8 cursor-pointer text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                         <path stroke-linecap="round" stroke-linejoin="round"
+                                                          d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z">
+                                                        </path>
+                                    </svg>
+                                    <!-- Estrela 4 -->
+                                    <svg data-estrela="4" class="estrela-btn w-8 h-8 cursor-pointer text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                          d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z">
+                                                        </path>
+                                    </svg>
+                                    <!-- Estrela 5 -->
+                                    <svg data-estrela="4" class="estrela-btn w-8 h-8 cursor-pointer text-gray-500" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24">
+                                         <path stroke-linecap="round" stroke-linejoin="round"
+                                                          d="M11.48 3.499a.562.562 0 011.04 0l2.125 5.111a.563.563 0 00.475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 00-.182.557l1.285 5.385a.562.562 0 01-.84.61l-4.725-2.885a.563.563 0 00-.586 0L6.982 20.54a.562.562 0 01-.84-.61l1.285-5.386a.562.562 0 00-.182-.557l-4.204-3.602a.563.563 0 01.321-.988l5.518-.442a.563.563 0 00.475-.345L11.48 3.5z">
+                                                        </path>
+                                    </svg>
+                                </div>
                             </div>
-                                
-                            <button type="submit" class="cursor-pointer col-span-1 text-white bg-blue-700 hover:bg-blue-800 px-4 py-2 rounded-lg">
+
+                            <button type="submit" class="mt-4 cursor-pointer col-span-1 text-white bg-blue-700 hover:bg-blue-800 px-4 py-2 rounded-lg">
                                 Salvar Avaliação
                             </button>
                         </form>
                     </div>
-                                
+
                     <script>
                     document.addEventListener("DOMContentLoaded", function () {
                         const estrelaBotoes = document.querySelectorAll('.estrela-btn');
@@ -308,11 +313,12 @@ $conexao->close();
                                 estrelasInput.value = valorSelecionado;
                             
                                 estrelaBotoes.forEach((b, i) => {
-                                    if (i < valorSelecionado) {
+                                    const bValor = parseInt(b.getAttribute('data-estrela'));
+                                    if (bValor <= valorSelecionado) {
                                         b.classList.add('text-yellow-600');
-                                        b.classList.remove('text-gray-600');
+                                        b.classList.remove('text-gray-500');
                                     } else {
-                                        b.classList.add('text-gray-600');
+                                        b.classList.add('text-gray-500');
                                         b.classList.remove('text-yellow-600');
                                     }
                                 });
@@ -320,6 +326,7 @@ $conexao->close();
                         });
                     });
                     </script>
+
 
 
             </div>
